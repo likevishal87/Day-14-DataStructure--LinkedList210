d19e1d71007adfbf0491b3c73c145b8fcdc5c43e
Vicky Verma <likevishalverma@gmail.com>
2022-11-14 12:24:03 +0530

    Added:Ability To Delete Last Element

---
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -0,0 +1,108 @@
+package com.bridgelabz.datastructure;
+
+public class LinkedList<T> {
+	public Node<T> head;
+	public Node<T> tail;
+
+	public void push(T data) {
+		Node<T> node = new Node<>(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			node.setNext(head);
+			head = node;
+		}
+	}
+
+	public void add(T data) {
+		Node<T> newnode = new Node<>(data);
+		if (head == null) {
+			head = newnode;
+			tail = newnode;
+		} else {
+			tail.setNext(newnode);
+			tail = newnode;
+		}
+	}
+
+	public Node<T> search(T searchData) {
+		Node<T> temp = head;
+		while (temp != null) {
+			if (temp.getData().equals(searchData))
+				return temp;
+			temp = (Node<T>) temp.getNext();
+		}
+		return null;
+	}
+
+	public boolean searchAndInsert(T searchData, T insertData) {
+		Node<T> newNode = new Node(insertData);
+		Node<T> searchedNode = search(searchData);
+		if (searchedNode == null)
+			return false;
+		else {
+			newNode.setNext(searchedNode.getNext());
+			searchedNode.setNext(newNode);
+			return true;
+		}
+
+	}
+
+	public T pop() {
+		if (head == null) {
+			return null;
+		} else {
+			T data = head.getData();
+			head = (Node<T>) head.getNext();
+			return data;
+		}
+	}
+
+	public void popFirst() {
+		if (head == null) {
+			System.out.println("Linked List is Empty");
+		} else {
+			T data = head.getData();
+			head = (Node<T>) head.getNext();
+			System.out.println(data);
+		}
+	}
+
+	public void popLast() {
+		if (head == null) {
+			System.out.println("Linked List is Empty");
+		} else {
+			Node<T> temp = head;
+			while (temp.getNext() != tail) {
+				temp = (Node<T>) temp.getNext();
+			}
+			T data = tail.getData();
+			temp.setNext(null);
+			tail = temp;
+			System.out.println(data);
+		}
+	}
+
+	public void show() {
+		if (head == null) {
+			System.out.println("List is Empty");
+		} else {
+			Node<T> temp = head;
+			while (temp != null) {
+				System.out.print(temp.getData() + " ");
+				temp = (Node<T>) temp.getNext();
+			}
+			System.out.println();
+		}
+	}
+
+	public boolean isEmpty() {
+		return head == null ? true : false;
+	}
+
+	@Override
+	public String toString() {
+		return "LinkedList{" + "head=" + head + '}';
+	}
+}
---
+++ src/com/bridgelabz/datastructure/LinkedListMain.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.datastructure;
+
+public class LinkedListMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to DataStructure linklist Program");
+		LinkedList<Integer> list = new LinkedList();
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		list.show();
+
+		LinkedList<Integer> list1 = new LinkedList<>();
+		list1.add(56);
+		list1.add(30);
+		list1.add(70);
+		list1.show();
+
+		LinkedList<Integer> list2 = new LinkedList<>();
+		list2.add(56);
+		list2.add(70);
+		list2.searchAndInsert(56, 30);
+		list2.show();
+		list2.pop();
+		list2.popFirst();
+		list2.show();
+		list2.popLast();
+		list2.show();
+	}
+}
